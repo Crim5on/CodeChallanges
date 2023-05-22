9838bdb5cba91238b6f98e57999eea87a700e26c
Sandro Schnetzer <sandro.schnetzer@qrios.se>
2023-05-22 11:08:36 +0200

    new try on long int division

--- CPP/Divide_integers_as_strings/main.cpp
+++ CPP/Divide_integers_as_strings/main.cpp
@@ -15,124 +15,159 @@
 #include <vector>
 #include <deque>
 
-void printDeque(const std::deque<unsigned short>& digits){
-    for(auto i : digits){
-        std::cout << i;
-    }
-    std::cout << std::endl;
-}
 
-static std::deque<unsigned short> digitStringToDeque(const std::string& digitString){
-    std::deque<unsigned short> digitDeque;
-    for(auto c : digitString){
-        digitDeque.push_back((unsigned short)(c - '0'));
-    }
-    return digitDeque;
-}
+/* HELPER FUNCTIONS START */
 
-
-static std::string digitDequeToString(std::deque<unsigned short>& digitDeque){
-    std::string digitString;
-    while(!digitDeque.empty()){
-        digitString.append(1, (char)(digitDeque.front() + '0'));
-        digitDeque.pop_front();
-    }
-    return digitString;
-}
-
-
-static void removeLeadingZeros(std::deque<unsigned short>& digits){
-    while((digits.front() == 0) && (digits.size() > 1)){
-        digits.pop_front();
+static inline void normalise(std::string& string){
+    while(string[0] == '0' && string.length() > 1){
+        string.erase(1);
     }
 }
 
-
-static bool isLower(const std::deque<unsigned short>& a, const std::deque<unsigned short>& b)
+static inline void patchLength(std::string& string, size_t requiredLength)
 {
-    if(a.size() < b.size()){
+    while (string.length() < requiredLength)
+    {
+        string.insert(string.begin(), '0');
+    }
+}
+
+
+static inline bool isGreaterOrEqual(std::string& a, std::string&b)
+{
+    normalise(a);
+    normalise(b);
+
+    if(a.length() > b.length()){
         return true;
     }
-    else if(a.size() > b.size()){
+    else if(a.length() < b.length()){
         return false;
-    } 
-    else if(a.size() == b.size()){
-        if(a == b){
+    }
+    for(size_t i=0; i<a.length(); i++){
+        // compare acii values
+        if(a[i] > b[i]){
+            return true;
+        }
+        else if(a[i] < b[i]){
             return false;
         }
-        for(size_t i=0; i<a.size(); i++){
-            if(a[i] > b[i]){
-                return false;
-            }
-            else if(a[i] < b[i]){
-                return true;
-            }
-            else{
-                ; // proceed.
-            }
+        else{
+            ;   // continue
         }
     }
-    return false;
+    return true; // if equal
 }
 
 
-static std::deque<unsigned short> minus(const std::deque<unsigned short>& minuend, const std::deque<unsigned short>& subtrahend)
+static inline bool isLesserOrEqual(std::string& a, std::string&b)
 {
-    std::deque<unsigned short> difference;
-    int i = minuend.size() - 1;
-    int j = subtrahend.size() - 1;
-    unsigned short ubertrag = 0;
-    unsigned short digit;
-    unsigned short subtrahend_digit;
+    normalise(a);
+    normalise(b);
 
-    while(i >= 0)
-    {
-        if(j >= 0){
-            subtrahend_digit = subtrahend[j] + ubertrag;
-            ubertrag = 0;
-            if(minuend[i] >= subtrahend_digit){
-                digit = minuend[i] - subtrahend_digit;
-            }
-            else{
-                digit = (minuend[i] + 10) - subtrahend_digit;
-                ubertrag = 1;
-            }
-            j--;
+    if(a.length() < b.length()){
+        return true;
+    }
+    else if(a.length() > b.length()){
+        return false;
+    }
+    for(size_t i=0; i<a.length(); i++){
+        // compare acii values
+        if(a[i] < b[i]){
+            return true;
+        }
+        else if(a[i] > b[i]){
+            return false;
         }
         else{
-            digit = minuend[i] - ubertrag;
-            ubertrag = 0;
+            ;   // continue
         }
-        difference.push_front(digit);
-        i--;
     }
-    removeLeadingZeros(difference); // can we remove this?
-    return difference;
+    return true; // if equal
+}
+
+static inline std::string minus(std::string& a, std::string& b)
+{
+    char digit_char; 
+    int digit_a, digit_b, digit_res, hold;
+    hold = 0;
+    std::string result;
+    patchLength(b, a.length());
+
+    // TODO: Fix formula.
+    for(long i=a.length()-1; i>=0; i--){
+        digit_a = a[i] - '0';
+        digit_b = b[i] - '0';
+
+        if((digit_a > digit_b) || (digit_a == digit_b && !hold)){
+            digit_res = digit_a - (digit_b + hold);
+            hold = 0;
+        }
+        else{
+            digit_res = 10 - (digit_b + hold) + digit_a;
+            hold = 1;
+        }
+        digit_char = digit_res + '0';
+        result.insert(result.begin(), digit_char);
+    }
+    return result;
 }
 
 
-std::vector<std::string> divide_strings(std::string a, std::string b){
-    int div_count = 0;
-    std::deque<unsigned short> remainder = digitStringToDeque(a);
-    std::deque<unsigned short> divisor = digitStringToDeque(b);
 
-    while (!isLower(remainder, divisor)){
-        remainder = minus(remainder, divisor);
-        div_count++;
+/* LOGIC IMPLEMENTATON START */
+
+std::vector<std::string> divide_strings(std::string a, std::string b)
+{
+    std::string frontDivident;
+    uint64_t divideCounter = 0;
+
+    for(size_t i=0; i<a.length(); i++){
+        frontDivident.push_back(a[i]);
+        if(isGreaterOrEqual(frontDivident, b)){
+            // frontDivident = frontDivident - b
+            // divide counter++
+        }
     }
-    
+
+
+
+    // vector<string> {quotient, remainder}
     std::vector<std::string> results;
-    results.push_back(std::to_string(div_count));
-    results.push_back(digitDequeToString(remainder));
     return results;
 }
 
 
+
+
+/* TEST CODE IMPLEMENTATION START */
+
 int main(void)
 {
-    std::vector<std::string> results = divide_strings("60", "5");
-    std::cout << "quotient: " << results[0] << std::endl;
-    std::cout << "remainder: " << results[1] << std::endl;
+    //std::vector<std::string> results = divide_strings("60", "5");
+    //std::cout << "quotient: " << results[0] << std::endl;
+    //std::cout << "remainder: " << results[1] << std::endl;
+
+    // define test strings:
+    std::string l5_v8 = "80000";
+    std::string l5_v4 = "40000";
+    std::string l6_v04 = "000400";
+    std::string l3_v8 = "800";
+
+    // testing greater than function:
+    std::cout << "80000 >= 40000: " << isGreaterOrEqual(l5_v8, l5_v4) << std::endl;
+    std::cout << "40000 >= 80000: " << isGreaterOrEqual(l5_v4, l5_v8) << std::endl;
+    std::cout << "80000 >= 80000: " << isGreaterOrEqual(l5_v8, l5_v8) << std::endl;
+    std::cout << "80000 >= 000400: " << isGreaterOrEqual(l5_v8, l6_v04) << std::endl;
+    std::cout << "80000 >= 800: " << isGreaterOrEqual(l5_v8, l3_v8) << std::endl;
+    std::cout << "800 >= 80000: " << isGreaterOrEqual(l3_v8, l5_v8) << std::endl;
+
+
+    // testing minus function:
+    std::string minuend = "0";
+    std::string subtrahend = "0";
+    std::cout << minuend << " - " << subtrahend << " = " << minus(minuend, subtrahend) << std::endl;
+
 
     return EXIT_SUCCESS;
 }
