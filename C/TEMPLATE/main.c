083ab53ffa2127690df22989b267ba9c810bf01f
Sandro Schnetzer <sandro.schnetzer@qrios.se>
2023-08-28 09:36:49 +0200

    added generic template to C folder

---
+++ C/TEMPLATE/main.c
@@ -0,0 +1,19 @@
+// challenge source: https://www.codewars.com/kata/.....
+/*
+    <description>
+*/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+
+
+int main (void) 
+{
+    printf("Hello World\n");
+
+    return EXIT_SUCCESS;
+}
